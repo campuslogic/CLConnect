32d81f2cd9ee3e5d7cd0fe6b2809cf2829cdb5d2
campuslogic <github@campuslogic.com>
2020-08-12 23:48:36 +0000

    CLConnect 2.12.8.36952

--- Global.asax.cs
+++ Global.asax.cs
@@ -3,6 +3,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 using log4net.Config;
 
 namespace CampusLogicEvents.Web
@@ -12,6 +13,8 @@ namespace CampusLogicEvents.Web
 
 	public class WebApiApplication : System.Web.HttpApplication
     {
+        private static readonly ILog logger = LogManager.GetLogger("AdoNetAppender");
+
         protected void Application_Start()
         {
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
@@ -26,6 +29,11 @@ namespace CampusLogicEvents.Web
 
             // Allow TLS 1.0, 1.1 and 1.2 for outbound requests
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+        }
+
+        protected void Application_End()
+        {
+            logger.Info("The application has been shut down.");
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CampusLogicEvents.Web")]
-[assembly: AssemblyDescription("Build: 2.12.8.36670 - Commit: 22810")]
+[assembly: AssemblyDescription("Build: 2.12.8.36952 - Commit: 23478")]
 [assembly: AssemblyConfiguration("release")]
 [assembly: AssemblyCompany("CampusLogic")]
 [assembly: AssemblyProduct("CL Connect")]
