2f4b6313fa498d839c4a41381350b3ab86509fdf
campuslogic <github@campuslogic.com>
2021-03-23 16:28:01 +0000

    CLConnect 2.12.13

--- Models/DataService.cs
+++ Models/DataService.cs
@@ -699,13 +699,37 @@ namespace CampusLogicEvents.Web.Models
                     }
 
                     // use the eventdata and its values to link up to the endpoint's parameters
-                    var parameterMappings = JArray.Parse(apiEndpoint.ParameterMappings);
-                    NameValueCollection eventParams = new NameValueCollection();
+                    JArray parameterMappings;
+                    try
+                    {
+                        parameterMappings = JArray.Parse(apiEndpoint.ParameterMappings);
+                    }
+                    catch (Exception)
+                    {
+                        logger.Error("apiEndpoint.ParameterMappings could not be parsed");
+
+                        throw;
+                    }
+
+                    if (!parameterMappings.Any())
+                    {
+                        throw new Exception("No parameter mappings were found");
+                    }
+
+                    var eventParams = new NameValueCollection();
 
                     // foreach mapping, get event property, find its corresponding eventdata, get that eventdata's value, attach it to the parameter in mapping
-                    foreach (JObject mapping in parameterMappings)
+                    foreach (JToken jToken in parameterMappings)
                     {
-                        var eventValue = eventData.PropertyValues[mapping["eventData"].Value<string>()].Value<string>();
+                        var mapping = (JObject)jToken;
+
+                        // This retrieves the "DisplayName" value of the EventProperty
+                        var mappingDisplayName = mapping["eventData"].Value<string>();
+
+                        // SV-3872 Convert to the "Name" value of the EventProperty
+                        // In cases of JS formula, this will apply that value instead
+                        var eventValue = eventData.GetValueByDisplayName(mappingDisplayName);
+
                         eventParams.Add(mapping["parameter"].Value<string>(), eventValue);
                     }
 
